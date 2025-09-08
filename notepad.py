6472d3353b920faf98e1649459862cdf87a7f9ec
jeonghyeon <sptlsrja@gmail.com>
2025-09-08 21:31:31 +0900

    fix: ip middleware's return message

--- notepad.py
+++ notepad.py
@@ -1,3 +1,6 @@
 # (window) PowerShell의 실행 정책(Execution Policy) 때문에 .ps1 스크립트를 실행하지 못할때
 # Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy RemoteSigned
 # .\venv\Scripts\Activate.ps1 
+
+# netstat -ano | findstr :9000
+# Get-Process -Id (Get-NetTCPConnection -LocalPort 9000).OwningProcess | Stop-Process -Force
